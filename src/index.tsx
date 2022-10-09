b16ae20e72b03532fd90e3d8cd327ddd4d30026d
kazuma1989 <kazuma.ebina@quipper.com>
2022-10-09 14:48:58 +0900

    firebase/app initializeAppだけ呼んだ

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { initializeApp } from "firebase/app"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { App } from "./App"
@@ -8,3 +9,13 @@ createRoot(globalThis.document.getElementById("root")!).render(
     <App />
   </StrictMode>
 )
+
+// Initialize Firebase
+const app = initializeApp({
+  apiKey: "AIzaSyB02s7OAFZJwB8npC9OTmFJPLFCz-Ax1Rk",
+  authDomain: "share-timer-2b51a.firebaseapp.com",
+  projectId: "share-timer-2b51a",
+  storageBucket: "share-timer-2b51a.appspot.com",
+  messagingSenderId: "996149287532",
+  appId: "1:996149287532:web:dd1dfe4c3ef583514bd649",
+})
