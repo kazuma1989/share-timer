2233d95e8ebef5f6d54ae11606102dfcfc6b67e6
kazuma1989 <kazuma.ebina@quipper.com>
2022-10-10 19:27:07 +0900

    modeではなくVITE_DB_VERSIONで明示的にdbバージョンを制御することにした

--- src/collection.ts
+++ src/collection.ts
@@ -9,23 +9,25 @@ export function collection(
   db: Firestore,
   ...paths: ["rooms"] | ["rooms", string, "actions"]
 ): CollectionReference {
-  return _collection(db, ...withModeSuffix(paths))
+  return _collection(db, ...withVersionSuffix(paths))
 }
 
-function withModeSuffix(paths: [string, ...string[]]): [string, ...string[]] {
-  return [`${paths[0]}-${import.meta.env.MODE}`, ...paths.splice(1)]
+function withVersionSuffix(
+  paths: [string, ...string[]]
+): [string, ...string[]] {
+  return [`${paths[0]}-${import.meta.env.VITE_DB_VERSION}`, ...paths.splice(1)]
 }
 
 if (import.meta.vitest) {
   const { test, expect } = import.meta.vitest
 
   test("basic", () => {
-    expect(withModeSuffix(["rooms"])).toStrictEqual(["rooms-test"])
+    expect(withVersionSuffix(["rooms"])).toStrictEqual(["rooms-dev"])
   })
 
   test("multiple paths", () => {
-    expect(withModeSuffix(["rooms", "xxx", "actions"])).toStrictEqual([
-      "rooms-test",
+    expect(withVersionSuffix(["rooms", "xxx", "actions"])).toStrictEqual([
+      "rooms-dev",
       "xxx",
       "actions",
     ])
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,4 @@
 // https://vitejs.dev/guide/env-and-mode.html#intellisense
+interface ImportMetaEnv {
+  VITE_DB_VERSION: string
+}
