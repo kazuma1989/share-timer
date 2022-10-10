8ecda63e3cac6cf4769851e4a108378aecd8b33a
kazuma1989 <kazuma.ebina@quipper.com>
2022-10-10 16:32:11 +0900

    できるだけpropsはoptionalにした
    APIなど外部から値が渡せないタイミングがどうしてもあって、そのとき最後の受け皿となるのがコンポーネントなので、コンポーネントでoptionalな値をハンドリングする。
    ただしUIをなんとかすればいいだけではない値（フックに使ったり状態を計算したりする）は、必須のまま残してる。

--- src/TimeViewer.tsx
+++ src/TimeViewer.tsx
@@ -4,11 +4,17 @@ import { useTimer } from "./useTimer"
 export function TimeViewer({
   duration,
   startedAt,
+  className,
 }: {
-  duration: number
-  startedAt: number
+  duration?: number
+  startedAt?: number
+  className?: string
 }) {
   const now = useTimer()
 
-  return <span>{formatDuration(duration - (now - startedAt))}</span>
+  return (
+    <span className={className}>
+      {formatDuration(duration && startedAt ? duration - (now - startedAt) : 0)}
+    </span>
+  )
 }
