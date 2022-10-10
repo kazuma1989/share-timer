526333cccb8d32cd4517514763dc4ff67b55d9e0
kazuma1989 <kazuma.ebina@quipper.com>
2022-10-10 14:03:59 +0900

    useRoomIdでhashにidがないときはroomの初期化をするように

--- src/useRoomId.ts
+++ src/useRoomId.ts
@@ -1,16 +1,51 @@
+import {
+  collection,
+  doc,
+  Firestore,
+  serverTimestamp,
+  writeBatch,
+} from "firebase/firestore"
+import { z } from "zod"
+import { timerAction } from "./timerAction"
+import { useFirestore } from "./useFirestore"
 import { useHash } from "./useHash"
 
 export function useRoomId(): string {
-  const roomId = useHash().slice("#".length)
+  const db = useFirestore()
 
-  if (!roomId) {
-    throw new Promise((resolve) => {
-      window.addEventListener("hashchange", resolve, {
-        passive: true,
-        once: true,
-      })
-    })
+  const roomId = useHash().slice("#".length)
+  if (roomId) {
+    return roomId
   }
 
-  return roomId
+  throw setupNewRoom(db).then((newRoomId) => {
+    window.location.hash = newRoomId
+  })
+}
+
+async function setupNewRoom(db: Firestore): Promise<string> {
+  const batch = writeBatch(db)
+
+  const rooms = collection(db, "rooms")
+  const newRoomId = doc(rooms).id
+  const newRoom = {}
+  batch.set(doc(rooms, newRoomId), {
+    ...newRoom,
+    createdAt: serverTimestamp(),
+  })
+
+  const actions = collection(rooms, newRoomId, "actions")
+  const newActionId = doc(actions).id
+  const newAction: z.input<typeof timerAction> = {
+    type: "edit-done",
+    duration: 3 * 60_000,
+  }
+  batch.set(doc(actions, newActionId), {
+    ...newAction,
+    createdAt: serverTimestamp(),
+  })
+
+  await batch.commit()
+
+  return newRoomId
 }
