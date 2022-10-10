58c2f8390677fb72532cd52f25a7635937d61f7a
kazuma1989 <kazuma.ebina@quipper.com>
2022-10-10 18:16:43 +0900

    開発のときはrooms-development、本番ではrooms-productionを使うようにした

--- src/collection.ts
+++ src/collection.ts
@@ -7,7 +7,27 @@ import {
 
 export function collection(
   db: Firestore,
-  ...path: ["rooms"] | ["rooms", string, "actions"]
+  ...paths: ["rooms"] | ["rooms", string, "actions"]
 ): CollectionReference {
-  return _collection(db, ...(path as [string, ...string[]]))
+  return _collection(db, ...withModeSuffix(paths))
+}
+
+function withModeSuffix(paths: [string, ...string[]]): [string, ...string[]] {
+  return [`${paths[0]}-${import.meta.env.MODE}`, ...paths.splice(1)]
+}
+
+if (import.meta.vitest) {
+  const { test, expect } = import.meta.vitest
+
+  test("basic", () => {
+    expect(withModeSuffix(["rooms"])).toStrictEqual(["rooms-test"])
+  })
+
+  test("multiple paths", () => {
+    expect(withModeSuffix(["rooms", "xxx", "actions"])).toStrictEqual([
+      "rooms-test",
+      "xxx",
+      "actions",
+    ])
+  })
 }
