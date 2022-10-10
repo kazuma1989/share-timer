6f050a062ed7c10385b6bec5157c09aab30b3054
kazuma1989 <kazuma.ebina@quipper.com>
2022-10-10 12:52:40 +0900

    firebaseOptionsを切り出した

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,11 @@ import { getFirestore } from "firebase/firestore"
 import { StrictMode, Suspense } from "react"
 import { createRoot } from "react-dom/client"
 import { App } from "./App"
+import { firebaseOptions } from "./firebaseOptions"
 import "./global.css"
 import { FirestoreProvider } from "./useFirestore"
 
-const firebaseApp = initializeApp({
-  apiKey: "AIzaSyB02s7OAFZJwB8npC9OTmFJPLFCz-Ax1Rk",
-  authDomain: "share-timer-2b51a.firebaseapp.com",
-  projectId: "share-timer-2b51a",
-  storageBucket: "share-timer-2b51a.appspot.com",
-  messagingSenderId: "996149287532",
-  appId: "1:996149287532:web:dd1dfe4c3ef583514bd649",
-})
-
+const firebaseApp = initializeApp(firebaseOptions)
 const firestore = getFirestore(firebaseApp)
 
 createRoot(globalThis.document.getElementById("root")!).render(
