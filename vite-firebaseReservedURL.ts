f962ed7c42970a66e13a3eed3418da4bca3193f5
kazuma1989 <kazuma.ebina@quipper.com>
2022-10-13 23:37:09 +0900

    vite previewでもproxyが使えるのだった
    バンドルに含める必要はなかった。

--- vite-firebaseReservedURL.ts
+++ vite-firebaseReservedURL.ts
@@ -1,14 +1,13 @@
 import * as fs from "node:fs/promises"
 import { Plugin } from "vite"
 
-interface Firebaserc {
-  projects: {
-    default: string
-  }
-}
-
 export function firebaseReservedURL(): Plugin {
-  const firebaserc = () =>
+  interface Firebaserc {
+    projects: {
+      default: string
+    }
+  }
+  const getFirebaserc = () =>
     fs
       .readFile("./.firebaserc", { encoding: "utf-8" })
       .then<Firebaserc>(JSON.parse)
@@ -18,7 +17,7 @@ export function firebaseReservedURL(): Plugin {
     name: "firebaseReservedURL",
 
     async config() {
-      const rc = await firebaserc()
+      const rc = await getFirebaserc()
       if (!rc) return
 
       return {
@@ -31,28 +30,6 @@ export function firebaseReservedURL(): Plugin {
           },
         },
       }
-    },
-
-    /**
-     * @see https://vitejs.dev/guide/api-plugin.html#universal-hooks
-     * @see https://rollupjs.org/guide/en/#generatebundle
-     * @see https://bundle-buddy.com/rollup
-     */
-    async generateBundle() {
-      const rc = await firebaserc()
-      if (!rc) return
-
-      // @ts-expect-error Node.js 18にはfetchが生えているはずだ
-      const initJSON = await fetch(
-        `https://${rc.projects.default}.web.app` + "/__/firebase/init.json"
-      ).then((_: any) => _.json())
-
-      // https://rollupjs.org/guide/en/#thisemitfile
-      this.emitFile({
-        type: "asset",
-        fileName: "__/firebase/init.json",
-        source: JSON.stringify(initJSON, null, 2),
-      })
     },
   }
 }
