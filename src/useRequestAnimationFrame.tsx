bfc3786acb3814c01703ef2c05dd5cbdebfd7ae6
kazuma1989 <kazuma.ebina@quipper.com>
2022-10-17 08:29:23 +0900

    subscribeTimerをやめてuseRequestAnimationFrameにした

---
+++ src/useRequestAnimationFrame.tsx
@@ -0,0 +1,29 @@
+import { useSyncExternalStore } from "react"
+import { now } from "./now"
+
+export function useRequestAnimationFrame<T>(
+  getSnapshot: (now: number) => T
+): T {
+  return useSyncExternalStore(subscribeTimer, () => getSnapshot(_now))
+}
+
+let _now = now()
+
+function subscribeTimer(next: () => void): () => void {
+  const abort = new AbortController()
+
+  const tick = () => {
+    if (abort.signal.aborted) return
+
+    requestAnimationFrame(tick)
+
+    _now = now()
+    next()
+  }
+
+  tick()
+
+  return () => {
+    abort.abort()
+  }
+}
