89e9f85b1ee55cf6a737f157e31e91f268626e6c
kazuma1989 <kazuma.ebina@quipper.com>
2022-10-16 23:50:01 +0900

    timerReducerとして切り出した
    useActionsのようにActionの配列を返してもreducerでTimerStateに変換するだけと気づいた。
    そのコンポーネントに閉じない使い方をしようとするので、まずは別ファイルに置いた。

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,11 +1,10 @@
 import { css } from "@emotion/css"
 import { serverTimestamp } from "firebase/firestore"
 import { useRef } from "react"
-import { Action } from "./actionZod"
 import { formatDuration } from "./formatDuration"
-import { now } from "./now"
 import { Room } from "./roomZod"
 import { timeInputZod } from "./timeInputZod"
+import { timerReducer } from "./timerReducer"
 import { TimeViewer } from "./TimeViewer"
 import { useActions, useDispatchAction } from "./useActions"
 import { useAllSettled } from "./useAllSettled"
@@ -28,7 +27,7 @@ export type TimerState =
 
 export function Timer({ roomId }: { roomId: Room["id"] }) {
   const actions = useActions(roomId)
-  const state = actions.reduce(reducer, {
+  const state = actions.reduce(timerReducer, {
     mode: "paused",
     restDuration: 0,
   })
@@ -149,171 +148,4 @@ export function Timer({ roomId }: { roomId: Room["id"] }) {
       )}
     </form>
   )
-}
-
-function reducer(state: TimerState, action: Action): TimerState {
-  switch (action.type) {
-    case "edit": {
-      if (state.mode !== "paused") {
-        return state
-      }
-
-      return {
-        mode: "editing",
-        initialDuration: state.restDuration,
-      }
-    }
-
-    case "edit-done": {
-      return {
-        mode: "paused",
-        restDuration: action.duration,
-      }
-    }
-
-    case "start": {
-      if (state.mode !== "paused") {
-        return state
-      }
-
-      return {
-        mode: "running",
-        duration: state.restDuration,
-        startedAt: action.at,
-      }
-    }
-
-    case "pause": {
-      if (state.mode !== "running") {
-        return state
-      }
-
-      return {
-        mode: "paused",
-        restDuration: state.duration - (action.at - state.startedAt),
-      }
-    }
-
-    // Do not use "default" here to be exhaustive for the all types.
-  }
-}
-
-if (import.meta.vitest) {
-  const { test, expect } = import.meta.vitest
-
-  test("edit", () => {
-    expect(
-      reducer(
-        {
-          mode: "paused",
-          restDuration: 5 * 60_000,
-        },
-        {
-          type: "edit",
-        }
-      )
-    ).toStrictEqual({
-      mode: "editing",
-      initialDuration: 5 * 60_000,
-    })
-  })
-
-  test("edit-done", () => {
-    expect(
-      reducer(
-        {
-          mode: "editing",
-          initialDuration: 3 * 60_000,
-        },
-        {
-          type: "edit-done",
-          duration: 5 * 60_000,
-        }
-      )
-    ).toStrictEqual({
-      mode: "paused",
-      restDuration: 5 * 60_000,
-    })
-  })
-
-  test("start", () => {
-    const _now = now()
-
-    expect(
-      reducer(
-        {
-          mode: "paused",
-          restDuration: 5 * 60_000,
-        },
-        {
-          type: "start",
-          at: _now,
-        }
-      )
-    ).toStrictEqual({
-      mode: "running",
-      duration: 5 * 60_000,
-      startedAt: _now,
-    })
-  })
-
-  test("pause", () => {
-    const _now = now()
-
-    expect(
-      reducer(
-        {
-          mode: "running",
-          duration: 5 * 60_000,
-          startedAt: _now - 40_000,
-        },
-        {
-          type: "pause",
-          at: _now,
-        }
-      )
-    ).toStrictEqual({
-      mode: "paused",
-      restDuration: 5 * 60_000 - 40_000,
-    })
-  })
-
-  test("multiple actions", () => {
-    const _now = now()
-    const actions: Action[] = [
-      {
-        type: "edit",
-      },
-      {
-        type: "edit-done",
-        duration: 7 * 60_000,
-      },
-      {
-        type: "start",
-        at: _now,
-      },
-      {
-        type: "pause",
-        at: _now + 10_000,
-      },
-      {
-        type: "start",
-        at: _now + 60_000,
-      },
-      {
-        type: "pause",
-        at: _now + 80_000,
-      },
-    ]
-
-    const state = actions.reduce(reducer, {
-      mode: "paused",
-      restDuration: 5 * 60_000,
-    })
-
-    expect(state).toStrictEqual({
-      mode: "paused",
-      restDuration: 6 * 60_000 + 30_000,
-    })
-  })
 }
