df8bcc27d1392a69dadc745f9b2cd53eca23fadb
kazuma1989 <kazuma.ebina@quipper.com>
2022-10-16 11:48:47 +0900

    calibrateClock関数を作った
    とりあえずactionsに書き込んでいるため、actionsのgetができなくてまだ動かない。

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,15 @@ import { createRoot } from "react-dom/client"
 import { App } from "./App"
 import { FullViewportProgress } from "./FullViewportProgress"
 import "./global.css"
+import { calibrateClock } from "./now"
 import { FirestoreProvider } from "./useFirestore"
 
 const firebaseApp = initializeApp(
   await fetch("/__/firebase/init.json").then((_) => _.json())
 )
 const firestore = getFirestore(firebaseApp)
+
+calibrateClock(firestore)
 
 createRoot(globalThis.document.getElementById("root")!).render(
   <StrictMode>
--- src/now.ts
+++ src/now.ts
@@ -1,7 +1,44 @@
+import {
+  doc,
+  Firestore,
+  getDocFromServer,
+  serverTimestamp,
+  setDoc,
+  Timestamp,
+} from "firebase/firestore"
+import { collection } from "./collection"
+import { withMeta } from "./withMeta"
+
 /**
  * クライアント時刻をなるべく較正してサーバー時刻に近づけた値を返す
  */
 export function now(): number {
   // eslint-disable-next-line no-restricted-globals
-  return Date.now()
+  return Date.now() + _estimatedDiff
+}
+
+/**
+ * serverTimestamp() - Date.now()
+ */
+let _estimatedDiff = 0
+
+export async function calibrateClock(db: Firestore): Promise<void> {
+  const clientDoc = doc(collection(db, "rooms", "____", "actions"))
+  await setDoc(
+    clientDoc,
+    // TODO zod type check
+    withMeta({
+      clientTime: Timestamp.now(),
+      serverTime: serverTimestamp(),
+    })
+  )
+
+  const serverDoc = await getDocFromServer(clientDoc)
+  // TODO zod validation
+  const { clientTime, serverTime } = serverDoc.data()! as {
+    clientTime: Timestamp
+    serverTime: Timestamp
+  }
+
+  _estimatedDiff = serverTime.toMillis() - clientTime.toMillis()
 }
