5093a45ecf2d06af2026de6b3a54f3d0c4308a5b
kazuma1989 <kazuma.ebina@quipper.com>
2022-10-17 16:29:37 +0900

    rxがどう動くか実験

--- src/index.tsx
+++ src/index.tsx
@@ -31,3 +31,108 @@ createRoot(globalThis.document.getElementById("root")!).render(
     </FirestoreProvider>
   </StrictMode>
 )
+
+import {
+  getDocs,
+  limitToLast,
+  onSnapshot,
+  query,
+  startAt,
+} from "firebase/firestore"
+import {
+  firstValueFrom,
+  fromEventPattern,
+  OperatorFunction,
+  pairwise,
+  share,
+  startWith,
+} from "rxjs"
+import { collection } from "./firestore/collection"
+import { orderBy } from "./firestore/orderBy"
+import { where } from "./firestore/where"
+
+const db = firestore
+const roomId = "A3VXRcrUtFVjyXQyuo9I"
+
+const Empty = Symbol("empty")
+type Empty = typeof Empty
+
+const x = fromEventPattern<number>(
+  (handler) => {
+    console.count("subscribe")
+
+    const abort = new AbortController()
+
+    getDocs(
+      query(
+        collection(db, "rooms", roomId, "actions"),
+        where("type", "==", "edit-done"),
+        orderBy("createdAt", "asc"),
+        limitToLast(1)
+      )
+    ).then((doc) => {
+      if (abort.signal.aborted) return
+
+      const _ = doc.docs[0]
+      const serverTimestampCommitted =
+        _ && !_.metadata.fromCache && !_.metadata.hasPendingWrites
+      const startAtLatestEditDone = serverTimestampCommitted ? [startAt(_)] : []
+
+      const unsubscribe = onSnapshot(
+        query(
+          collection(db, "rooms", roomId, "actions"),
+          orderBy("createdAt", "asc"),
+          ...startAtLatestEditDone
+        ),
+        (doc) => {
+          console.debug("listen %d docChanges", doc.docChanges().length)
+
+          if (doc.size === 1) {
+            console.log(doc.docs.map((d) => d.data()))
+          }
+          handler(doc.size)
+        }
+      )
+
+      abort.signal.addEventListener("abort", unsubscribe)
+    })
+
+    return () => {
+      abort.abort()
+    }
+  },
+  (_, abort) => {
+    console.count("abort")
+    abort()
+  }
+).pipe(
+  startWith(Empty),
+  pairwise() as OperatorFunction<number | Empty, [number | Empty, number]>,
+  share()
+)
+
+firstValueFrom(x).then((_) => {
+  console.log("promise 1", _)
+
+  const s = x.subscribe((_) => {
+    console.log("in promise 1", _)
+  })
+})
+
+// firstValueFrom(x)
+
+{
+  const s = x.subscribe((_) => {
+    // console.log("1", _)
+  })
+  // s.unsubscribe()
+}
+
+// globalThis.setTimeout(() => {
+//   {
+//     const s = x.subscribe((_) => {
+//       console.log("2", _)
+//     })
+//     // s.unsubscribe()
+//   }
+// }, 2_000)
