c1ca761f11c2566d6a7ad83036cb7b7ee6e4531d
kazuma1989 <kazuma.ebina@quipper.com>
2022-10-22 18:57:12 +0900

    実験: iOS Safari: ユーザーが自発的に鳴らしたaudioインスタンスなら、その後スクリプトで鳴らしてもうまく鳴るらしい

--- src/CheckAudioButton.tsx
+++ src/CheckAudioButton.tsx
@@ -3,6 +3,7 @@ import { CircleButton } from "./CircleButton"
 import smallAlert from "./sound/small-alert.mp3"
 import { checkAudioPermission } from "./util/checkAudioPermission"
 import { createStore } from "./util/createStore"
+import { setTimeout } from "./util/setTimeout"
 
 export function CheckAudioButton() {
   const getStore = () => createStore(checkAudioPermission(smallAlert), "denied")
@@ -11,7 +12,27 @@ export function CheckAudioButton() {
   const permission = useSyncExternalStore(store.subscribe, store.getSnapshot)
 
   return permission === "canplay" ? (
-    <CircleButton>なる</CircleButton>
+    <CircleButton
+      onClick={async () => {
+        const ctx = new AudioContext()
+        const osc = ctx.createOscillator()
+        osc.type = "sine"
+        osc.connect(ctx.destination)
+        osc.start()
+        osc.stop(1)
+
+        const audio = new Audio(smallAlert)
+        audio.play()
+
+        await setTimeout(1_000)
+
+        audio.pause()
+        audio.currentTime = 0
+        audio.play()
+      }}
+    >
+      なる
+    </CircleButton>
   ) : (
     <CircleButton
       onClick={() => {
