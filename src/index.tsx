678c6c7cf18ed29f7cd6d9cdab3a513db98a6784
kazuma1989 <kazuma.ebina@quipper.com>
2022-10-22 22:30:26 +0900

    一度でも画面をタップすれば音が鳴るように仕込んだ

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import { calibrateClock } from "./now"
 import smallAlert from "./sound/small-alert.mp3"
 import { AlertAudioProvider } from "./useAlertAudio"
 import { FirestoreProvider } from "./useFirestore"
+import { checkAudioPermission } from "./util/checkAudioPermission"
 
 const firestore = await initializeFirestore()
 
@@ -15,10 +16,26 @@ calibrateClock(firestore).catch((reason) => {
   console.warn("calibration failed", reason)
 })
 
-createRoot(globalThis.document.getElementById("root")!).render(
+const audio = new Audio(smallAlert)
+
+document.body.addEventListener(
+  "click",
+  async () => {
+    const permission = await checkAudioPermission(audio)
+    if (permission === "denied") {
+      console.warn("Cannot play audio")
+    }
+  },
+  {
+    passive: true,
+    once: true,
+  }
+)
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <FirestoreProvider value={firestore}>
-      <AlertAudioProvider value={new Audio(smallAlert)}>
+      <AlertAudioProvider value={audio}>
         <Suspense fallback={<FullViewportProgress />}>
           <App />
         </Suspense>
