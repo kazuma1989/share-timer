d179b82ef169cba8e5dfd739fd06d62e4198dd1a
kazuma1989 <kazuma.ebina@quipper.com>
2022-10-23 22:44:46 +0900

    なるべくcreateStoreよりuseObservableを使う
    createStoreはinternalな感じがするため。

--- src/useAllSettled.ts
+++ src/useAllSettled.ts
@@ -1,13 +1,16 @@
-import { Reducer, useReducer, useSyncExternalStore } from "react"
-import { createStore, Store } from "./util/createStore"
+import { Reducer, useReducer } from "react"
+import { useObservable } from "./useObservable"
 
 interface Add {
   <T extends PromiseLike<unknown>>(promise: T): T
 }
 
 export function useAllSettled(): [allSettled: boolean, add: Add] {
-  const [[, store], dispatch] = useReducer<
-    Reducer<[Set<PromiseLike<unknown>>, Store<boolean>], PromiseLike<unknown>>
+  const [[, promise], dispatch] = useReducer<
+    Reducer<
+      [Set<PromiseLike<unknown>>, PromiseLike<boolean>],
+      PromiseLike<unknown>
+    >
   >(
     (state, promise) => {
       const [currentPromises] = state
@@ -18,19 +21,16 @@ export function useAllSettled(): [allSettled: boolean, add: Add] {
       const newPromises = new Set(currentPromises.values()).add(promise)
       return [
         newPromises,
-        createStore(
-          Promise.allSettled(newPromises).then(() => {
-            newPromises.clear()
-            return true
-          }),
-          false
-        ),
+        Promise.allSettled(newPromises).then(() => {
+          newPromises.clear()
+          return true
+        }),
       ]
     },
-    [new Set(), createStore(Promise.resolve(true), true)]
+    [new Set(), Promise.resolve(true)]
   )
 
-  const allSettled = useSyncExternalStore(store.subscribe, store.getSnapshot)
+  const allSettled = useObservable(promise, true)
 
   const add: Add = (promise) => {
     dispatch(promise)
