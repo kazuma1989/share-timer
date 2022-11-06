fa33d63bdb7dec046ffa63be0a38dce5c302b433
kazuma1989 <kazuma.ebina@quipper.com>
2022-11-06 09:44:30 +0900

    エラー情報を手に入れたりリセットしたりできるようになった

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -1,12 +1,22 @@
 import { Component, ErrorInfo, ReactNode } from "react"
+import { createContext } from "./createContext"
+
+export const [ErrorProvider, useError] = createContext<unknown>("ErrorProvider")
+
+interface ResetError {
+  (): void
+}
+
+export const [ResetErrorProvider, useResetError] =
+  createContext<ResetError>("ResetErrorProvider")
 
 interface Props {
-  fallback: ReactNode
+  fallback?: ReactNode
   children?: ReactNode
 }
 
 interface State {
-  error: unknown
+  error: unknown | null
 }
 
 /**
@@ -14,24 +24,42 @@ interface State {
  */
 export class ErrorBoundary extends Component<Props, State> {
   static getDerivedStateFromError(error: unknown): State {
-    return { error }
+    return {
+      error,
+    }
   }
 
   constructor(props: Props) {
     super(props)
 
-    this.state = { error: null }
+    this.state = {
+      error: null,
+    }
   }
 
   componentDidCatch(error: unknown, errorInfo: ErrorInfo): void {
     console.warn(error, errorInfo)
   }
 
+  private resetError: ResetError = () => {
+    this.setState({
+      error: null,
+    })
+  }
+
   render() {
-    if (this.state.error) {
-      return this.props.fallback || null
+    const { fallback, children } = this.props
+    const { error } = this.state
+    const { resetError } = this
+
+    if (error) {
+      return (
+        <ResetErrorProvider value={resetError}>
+          <ErrorProvider value={error}>{fallback}</ErrorProvider>
+        </ResetErrorProvider>
+      )
     }
 
-    return this.props.children ?? null
+    return children ?? null
   }
 }
