5babda3d1a10c77f71efe9dbe4c0d639f5c40298
kazuma1989 <kazuma.ebina@quipper.com>
2022-11-12 13:52:22 +0900

    initializeRoom -> restoreRoom

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,12 @@ import { FullViewportOops } from "./FullViewportOops"
 import { FullViewportProgress } from "./FullViewportProgress"
 import "./global.css"
 import { initializeFirestore } from "./initializeFirestore"
-import { initializeRoom } from "./initializeRoom"
 import { isRoom, mapToRoom } from "./mapToRoom"
 import { mapToRoomId, mapToRoute } from "./mapToRoute"
 import { calibrateClock } from "./now"
 import { observeHash } from "./observeHash"
 import { observeMediaPermission } from "./observeMediaPermission"
+import { restoreRoom } from "./restoreRoom"
 import smallAlert from "./sound/small-alert.mp3"
 import { AudioProvider, MediaPermissionProvider } from "./useAudio"
 import { FirestoreProvider } from "./useFirestore"
@@ -35,7 +35,7 @@ const [room$, invalid$] = partition(
   isRoom
 )
 
-initializeRoom(firestore, invalid$)
+restoreRoom(firestore, invalid$)
 
 createRoot(root).render(
   <StrictMode>
--- src/restoreRoom.ts
+++ src/restoreRoom.ts
@@ -8,7 +8,7 @@ import { sparse } from "./util/sparse"
 import { ActionOnFirestore } from "./zod/actionZod"
 import { RoomOnFirestore } from "./zod/roomZod"
 
-export function initializeRoom(
+export function restoreRoom(
   db: Firestore,
   invalid$: Observable<InvalidDoc>
 ): void {
