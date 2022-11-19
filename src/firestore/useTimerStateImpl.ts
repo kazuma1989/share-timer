5ea11a8e9cd2dba9814e0b23b638bb3e1c3efe37
kazuma1989 <kazuma.ebina@quipper.com>
2022-11-19 15:38:42 +0900

    mapToTimerStateをuseTimerStateImplに統合

--- src/firestore/useTimerStateImpl.ts
+++ src/firestore/useTimerStateImpl.ts
@@ -1,9 +1,23 @@
-import { distinctUntilChanged, map, Observable } from "rxjs"
-import { mapToTimerState } from "../mapToTimerState"
-import { TimerState } from "../timerReducer"
+import {
+  getDocs,
+  limitToLast,
+  query,
+  queryEqual,
+  startAt,
+} from "firebase/firestore"
+import { distinctUntilChanged, map, Observable, switchMap } from "rxjs"
+import { timerReducer, TimerState } from "../timerReducer"
 import { createCache } from "../util/createCache"
+import { safeParseDocsWith } from "../util/safeParseDocsWith"
+import { shareRecent } from "../util/shareRecent"
+import { snapshotOf } from "../util/snapshotOf"
+import { actionZod } from "../zod/actionZod"
 import { Room } from "../zod/roomZod"
+import { collection } from "./collection"
+import { hasNoEstimateTimestamp } from "./hasNoEstimateTimestamp"
+import { orderBy } from "./orderBy"
 import { useFirestore } from "./useFirestore"
+import { where } from "./where"
 
 export function useTimerStateImpl(
   room$: Observable<Room>
@@ -14,7 +28,44 @@ export function useTimerStateImpl(
     room$.pipe(
       map((_) => _.id),
       distinctUntilChanged(),
-      mapToTimerState(db)
+      switchMap((roomId) =>
+        getDocs(
+          query(
+            collection(db, "rooms", roomId, "actions"),
+            where("type", "==", "start"),
+            orderBy("createdAt", "asc"),
+            limitToLast(1)
+          )
+        ).then(({ docs: [doc] }) =>
+          query(
+            collection(db, "rooms", roomId, "actions"),
+            orderBy("createdAt", "asc"),
+            ...(hasNoEstimateTimestamp(doc?.metadata) ? [startAt(doc)] : [])
+          )
+        )
+      ),
+
+      distinctUntilChanged(queryEqual),
+      switchMap((selectActions) => {
+        const parseDocs = safeParseDocsWith(actionZod)
+
+        console.debug("actions listener attached")
+
+        return snapshotOf(selectActions).pipe(
+          map((snapshot) => {
+            console.debug("listen %d docChanges", snapshot.docChanges().length)
+
+            const actions = parseDocs(snapshot.docs)
+
+            return actions.reduce(timerReducer, {
+              mode: "editing",
+              initialDuration: 0,
+            })
+          })
+        )
+      }),
+
+      shareRecent(30_000)
     )
   )
 
