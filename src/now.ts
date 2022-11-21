41bdbc5028209e8d0f160a5e2997c24c70b0319f
kazuma1989 <kazuma.ebina@quipper.com>
2022-11-22 08:38:52 +0900

    calibrateClockもfirestore依存なので分離した

--- src/now.ts
+++ src/now.ts
@@ -1,15 +1,3 @@
-import {
-  doc,
-  Firestore,
-  getDocFromServer,
-  serverTimestamp,
-  setDoc,
-  Timestamp,
-} from "firebase/firestore"
-import { collection } from "./firestore/collection"
-import { withMeta } from "./firestore/withMeta"
-import { Calibration, calibrationZod } from "./zod/calibrationZod"
-
 /**
  * クライアント時刻をなるべく較正してサーバー時刻に近づけた値を返す
  */
@@ -17,31 +5,22 @@ export function now(): number {
   return _now() + _estimatedDiff
 }
 
-// eslint-disable-next-line no-restricted-globals
-const _now = () => Date.now() + delta
-const delta = import.meta.env.DEV
-  ? Math.floor(4_000 * Math.random() - 2_000)
-  : 0
+const fakeDelta = Math.floor(4_000 * Math.random() - 2_000)
+const _now = import.meta.env.DEV
+  ? () =>
+      // eslint-disable-next-line no-restricted-globals
+      Date.now() + fakeDelta
+  : () =>
+      // eslint-disable-next-line no-restricted-globals
+      Date.now()
 
 /**
  * serverTimestamp() - Timestamp.now()
  */
 let _estimatedDiff = 0
 
-export async function calibrateClock(db: Firestore): Promise<void> {
-  const clientDoc = doc(collection(db, "calibrations"))
-  await setDoc(
-    clientDoc,
-    withMeta<Calibration>({
-      clientTime: Timestamp.fromMillis(_now()),
-      serverTime: serverTimestamp() as Timestamp,
-    })
-  )
-
-  const serverDoc = await getDocFromServer(clientDoc)
-  const { clientTime, serverTime } = calibrationZod.parse(serverDoc.data())
-
-  _estimatedDiff = serverTime.toMillis() - clientTime.toMillis()
+export function setEstimatedDiff(value: number): void {
+  _estimatedDiff = value
 
   import.meta.env.DEV &&
     console.info("diff (serverTime - clientTime)", _estimatedDiff)
