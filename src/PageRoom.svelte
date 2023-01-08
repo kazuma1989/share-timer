f1932e07eb43dee0f3d2f3a1bb763a79876217d4
kazuma1989 <kazuma.ebina@quipper.com>
2023-01-09 01:02:37 +0900

    InvalidDoc の場合をモックして試してみた

--- src/PageRoom.svelte
+++ src/PageRoom.svelte
@@ -1,13 +1,29 @@
-<script lang="ts">
+<script lang="ts" context="module">
+  import { map, merge, partition, type Observable } from "rxjs"
   import { useRoom } from "./useRoom.1"
-  import type { Room } from "./zod/roomZod"
+  import { isRoom, type InvalidDoc, type Room } from "./zod/roomZod"
 
-  export let roomId: Room["id"]
+  function roomOrInvalid(_room$: Observable<Room | InvalidDoc>) {
+    const [room$, _invalid$] = partition(_room$, isRoom)
+    const invalid$ = merge(room$.pipe(map(() => null)), _invalid$)
 
-  $: room$ = useRoom(roomId)
-  $: console.log($room$)
+    return [room$, invalid$] as const
+  }
 </script>
 
-<div class="mx-auto h-screen max-w-prose">
-  <p>ROOM ({roomId})</p>
-</div>
+<script lang="ts">
+  export let roomId: Room["id"]
+
+  $: [room$, invalid$] = roomOrInvalid(useRoom(roomId))
+
+  $: if ($invalid$) {
+    const [, invalidRoomId] = $invalid$
+    console.log("invalid", invalidRoomId)
+  }
+</script>
+
+{#if $room$}
+  <div class="mx-auto h-screen max-w-prose">
+    <p>ROOM ({$room$.id})</p>
+  </div>
+{/if}
--- src/firestore/firestoreImplContext.ts
+++ src/firestore/firestoreImplContext.ts
@@ -1,12 +1,15 @@
 import { of } from "rxjs"
 import { keyWithUseRoom } from "../useRoom.1"
-import type { Room } from "../zod/roomZod"
+import type { InvalidDoc, Room } from "../zod/roomZod"
 
 export const firestoreImplContext: Map<unknown, unknown> = new Map([
   keyWithUseRoom((roomId) =>
-    of({
-      id: roomId,
-      name: `another mocked room (${Math.random()})`,
-    } satisfies Room)
+    // TODO 本物の実装を与える
+    Math.random() < 0.3
+      ? of({
+          id: roomId,
+          name: `another mocked room (${Math.random()})`,
+        } satisfies Room)
+      : of(["invalid-doc", roomId] satisfies InvalidDoc)
   ),
 ])
