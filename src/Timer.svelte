aa02b72d32e3d7691253b9f68589b659ea0df6b0
kazuma1989 <kazuma.ebina@quipper.com>
2023-01-09 14:24:35 +0900

    sr-only の label を追加

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import clsx from "clsx"
-  import type { Observable } from "rxjs"
+  import { map, type Observable } from "rxjs"
+  import { now } from "./now"
   import type { TimerState } from "./timerReducer"
   import { getId } from "./util/getId"
+  import { humanReadableLabelOf } from "./util/humanReadableLabelOf"
   import type { Room } from "./zod/roomZod"
 
   export let room$: Observable<Room>
@@ -13,6 +15,30 @@
 
   $: ({ id: roomId, name: roomName } = $room$ ?? {})
 
+  $: label$ = timerState$.pipe(
+    map((state) => {
+      switch (state.mode) {
+        case "editing": {
+          return `タイマーは編集中です。値は${humanReadableLabelOf(
+            state.initialDuration
+          )}`
+        }
+
+        case "running": {
+          return `タイマーは実行中です。残り${humanReadableLabelOf(
+            state.restDuration - (now() - state.startedAt)
+          )}`
+        }
+
+        case "paused": {
+          return `タイマーは一時停止中です。残り${humanReadableLabelOf(
+            state.restDuration
+          )}`
+        }
+      }
+    })
+  )
+
   const _id = getId()
   const id = (_: "timer" | "status") => _id + _
 </script>
@@ -22,8 +48,14 @@
     <h1 aria-label={`タイマーの名前: ${roomName}`}>{roomName}</h1>
   </div>
 
-  <form class="contents">
-    <p id={id("status")} role="status" class="sr-only" />
+  <form
+    class="contents"
+    on:submit|preventDefault={() => {
+      // TODO 本物の実装
+      alert("not implemented!")
+    }}
+  >
+    <p id={id("status")} role="status" class="sr-only">{$label$}</p>
 
     <div id={id("timer")} class="grid place-items-center tabular-nums" />
   </form>
