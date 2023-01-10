82c2e83c4c91c2483d6f86e6e403b73b0ec81f4c
kazuma1989 <kazuma.ebina@quipper.com>
2023-01-10 10:11:35 +0900

    最低限の Slider を実装した

--- src/Slider.svelte
+++ src/Slider.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import clsx from "clsx"
+  import type { Action } from "svelte/types/runtime/action"
+
   export let label: string = ""
   export let value: number = 0
   export let valueMax: number = 0
@@ -6,8 +9,44 @@
   let className: string = ""
   export { className as class }
 
-  // TODO valueNow の役割ってなんだっけ
-  $: valueNow = value
+  const observe: Action<HTMLElement> = (root) => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const [target] = entries
+          .filter(
+            (
+              _
+            ): _ is Omit<IntersectionObserverEntry, "target"> & {
+              target: HTMLElement
+            } => _.isIntersecting && _.target instanceof HTMLElement
+          )
+          .map((_) => _.target)
+
+        if (target) {
+          root.dispatchEvent(
+            new CustomEvent("intersect", {
+              detail: target,
+            })
+          )
+        }
+      },
+      {
+        threshold: 1,
+        rootMargin: "-32px 0px",
+        root,
+      }
+    )
+
+    Array.from(root.children).forEach((child) => {
+      observer.observe(child)
+    })
+
+    return {
+      destroy() {
+        observer.disconnect()
+      },
+    }
+  }
 </script>
 
 <span
@@ -16,8 +55,8 @@
   aria-orientation="vertical"
   aria-valuemin={0}
   aria-valuemax={valueMax}
-  aria-valuenow={valueNow}
-  aria-valuetext={valueNow === undefined ? undefined : `${valueNow}${label}`}
+  aria-valuenow={value}
+  aria-valuetext={value === undefined ? undefined : `${value}${label}`}
   tabindex="0"
   class={className}
   on:keydown={(e) => {
@@ -48,5 +87,32 @@
     }
   }}
 >
-  {value}{label}
+  <span
+    class={clsx(
+      "scrollbar-hidden inline-flex snap-y snap-mandatory flex-col overflow-y-scroll overscroll-contain [&>*]:snap-center",
+      "h-[calc(36px+6rem)] px-4 [&>:first-child]:mt-12 [&>:last-child]:mb-12",
+      "cursor-pointer select-none rounded-md transition-colors",
+      "hover:bg-dark/10 dark:hover:bg-light/20",
+      "-mr-12 pr-14 text-3xl"
+    )}
+    use:observe
+    on:intersect={(e) => {
+      if (!(e.detail instanceof HTMLElement)) return
+
+      value = Number(e.detail.dataset.value)
+    }}
+  >
+    {#each Array(valueMax + 1) as _, index (index)}
+      <span
+        data-value={index}
+        class={clsx("text-right", index !== value && "font-thin opacity-25")}
+      >
+        {index.toString(10).padStart(2, "0")}
+      </span>
+    {/each}
+  </span>
+
+  <span class="pointer-events-none inline-block w-12 pr-2 text-right text-lg">
+    {label}
+  </span>
 </span>
--- src/global.d.ts
+++ src/global.d.ts
@@ -3,3 +3,10 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
 /// <reference types="vitest/importMeta" />
+
+// https://github.com/sveltejs/language-tools/blob/84c7463def79e88550aa12074830ed7d7945e1ee/docs/preprocessors/typescript.md#im-using-an-attributeevent-on-a-dom-element-and-it-throws-a-type-error
+declare namespace svelteHTML {
+  interface HTMLAttributes<T> {
+    "on:intersect"?: (event: CustomEvent) => any
+  }
+}
