fb7b563be3fab507cdd2e3b88ce74b09cb652873
kazuma1989 <kazuma.ebina@quipper.com>
2023-01-15 14:51:24 +0900

    FlashCover を Svelte に移植

--- src/FlashCover.svelte
+++ src/FlashCover.svelte
@@ -1,8 +1,31 @@
 <script lang="ts">
-  import type { Observable } from "rxjs"
+  import clsx from "clsx"
+  import { map, Observable, withLatestFrom } from "rxjs"
+  import { mapToCurrentDuration } from "./mapToCurrentDuration"
+  import { notifyFirstZero } from "./notifyFirstZero"
+  import { now } from "./now"
   import type { TimerState } from "./timerReducer"
+  import { useConfig } from "./useConfig"
+  import { interval } from "./util/interval"
 
   export let timerState$: Observable<TimerState>
+  export { className as class }
+  let className: string = ""
+
+  const config$ = useConfig()
+
+  $: flashing$ = timerState$.pipe(
+    mapToCurrentDuration(interval("ui"), now),
+    notifyFirstZero(),
+    withLatestFrom(config$),
+    map(([_, config]) => config.flash === "on" && _)
+  )
 </script>
 
-<!-- TODO 本物の実装 -->
+<div
+  class={clsx(
+    "pointer-events-none absolute inset-0 text-cerise-500/75 dark:text-gray-100/75",
+    $flashing$ && "animate-flash",
+    className
+  )}
+/>
