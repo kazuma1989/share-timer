b1b5e859b79de55020f24546b6c82a5b3eec1098
kazuma1989 <kazuma.ebina@quipper.com>
2023-02-06 13:27:54 +0900

    Firestore にも書き込めるようになった
    Emulators を別プロセスで起動していると本番プロジェクトに書き込んでしまう

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
-// import * as admin from "firebase-admin"
+import * as admin from "firebase-admin"
 import * as functions from "firebase-functions"
 
-// const app = admin.initializeApp()
+const app = admin.initializeApp()
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -9,4 +9,18 @@ import * as functions from "firebase-functions"
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true })
   response.send("Hello from Firebase!")
+})
+
+export const addMessage = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text
+
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const writeResult = await app
+    .firestore()
+    .collection("messages")
+    .add({ original: original })
+
+  // Send back a message that we've successfully written the message
+  res.json({ result: `Message with ID: ${writeResult.id} added.` })
 })
