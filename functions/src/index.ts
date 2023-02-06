2a1a03af059419fd3ce35b36371ce51329e25a2a
kazuma1989 <kazuma.ebina@quipper.com>
2023-02-06 15:51:48 +0900

    setCustomUserClaims でアプリ固有の情報をもたせられた
    ガバガバ GET API なので実験用途 only

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,29 @@ import * as functions from "firebase-functions"
 
 const app = admin.initializeApp()
 
+// http://127.0.0.1:5001/share-timer-2b51a/us-central1/addCustomClaims?uid=xxx
+export const addCustomClaims = functions.https.onRequest(async (req, res) => {
+  const { uid } = req.query
+  if (typeof uid !== "string") {
+    res.status(400).send()
+    return
+  }
+
+  functions.logger.info(
+    "customClaims",
+    (await app.auth().getUser(uid)).customClaims
+  )
+
+  await app.auth().setCustomUserClaims(uid, {
+    app_v1: {
+      id: "kazuma1989",
+      plan: "premium",
+    },
+  })
+
+  res.status(200).json((await app.auth().getUser(uid)).customClaims)
+})
+
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
