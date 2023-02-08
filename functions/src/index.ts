a26974064dbe83586e004e453361c93e42016777
kazuma1989 <kazuma.ebina@quipper.com>
2023-02-08 18:12:09 +0900

    Stripe 署名を検証してみた
    secrets は
    
    ```
    npx firebase functions:secrets:set STRIPE_API_KEY
    ```
    
    のようなコマンドで追加する
    ローカルでは .env.local の値が使われる

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,13 +2,34 @@ import { initializeApp } from "firebase-admin/app"
 import { getAuth } from "firebase-admin/auth"
 import { getFirestore } from "firebase-admin/firestore"
 import * as functions from "firebase-functions"
+import Stripe from "stripe"
 import * as s from "superstruct"
+
+declare const process: {
+  env: {
+    STRIPE_API_KEY: string
+    STRIPE_ENDPOINT_SECRET: string
+  }
+}
+
+const secrets = [
+  "STRIPE_API_KEY",
+  "STRIPE_ENDPOINT_SECRET",
+] satisfies (keyof typeof process.env)[]
+
+const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+  apiVersion: "2022-11-15",
+})
 
 interface CheckoutSession extends s.Infer<typeof checkoutSessionSchema> {}
 
 const checkoutSessionSchema = s.type({
   id: s.string(),
   uid: s.string(),
+})
+
+const stripeHeadersSchema = s.type({
+  "stripe-signature": s.string(),
 })
 
 const app = initializeApp()
@@ -23,13 +44,27 @@ function document<T extends "checkout-sessions-v1/{id}">(path: T): T {
   return path
 }
 
-export const checkoutSessionCompleted = functions.https.onRequest(
-  async (req, res) => {
+export const checkoutSessionCompleted = functions
+  .runWith({ secrets })
+  .https.onRequest(async (req, res) => {
     if (req.method !== "POST") {
       res.setHeader("Allow", "POST")
       res.status(405).send()
       return
     }
+
+    if (!s.is(req.headers, stripeHeadersSchema)) {
+      res.status(400).send()
+      return
+    }
+
+    const event = stripe.webhooks.constructEvent(
+      req.rawBody,
+      req.headers["stripe-signature"],
+      process.env.STRIPE_ENDPOINT_SECRET
+    )
+
+    functions.logger.debug(event.type)
 
     const [error, data] = s.validate(req.body, checkoutSessionSchema)
     if (error) {
@@ -49,8 +84,7 @@ export const checkoutSessionCompleted = functions.https.onRequest(
     } catch {
       res.status(409).json(data)
     }
-  }
-)
+  })
 
 export const onWriteCheckoutSession = functions.firestore
   .document(document("checkout-sessions-v1/{id}"))
