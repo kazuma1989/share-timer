183f9c4853357e008cb2617032d68e34e194eeb7
kazuma1989 <kazuma.ebina@quipper.com>
2023-02-13 09:48:21 +0900

    不要になった createCheckoutSession を削除
    組み込み料金表を使うようにしたので。
    実装すべきものは少ないほうがいい。
    クラシカルな POST form は勉強としては役立った（CSRF の理解ができたなど）。
    hidden input の値はどうせスクリプトから設定しないといけなかったし、HTML だけで完結はしなかった。

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,5 @@ import { initializeApp } from "firebase-admin/app"
 
 initializeApp()
 
-export * from "./createCheckoutSession"
 export * from "./onWriteCheckoutSession"
 export * from "./stripeWebhook"
