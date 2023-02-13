a4cf69a0fe1c8a2cb724138806fab6e72015fcb5
kazuma1989 <kazuma.ebina@quipper.com>
2023-02-13 09:59:35 +0900

    使わなくなった params.ts と .env を削除

--- functions/src/params.ts
+++
@@ -1,4 +0,0 @@
-import { defineString } from "firebase-functions/params"
-
-export const STRIPE_PRICE_API_ID$ = defineString("STRIPE_PRICE_API_ID")
-export const HOSTING_ORIGIN$ = defineString("HOSTING_ORIGIN")
