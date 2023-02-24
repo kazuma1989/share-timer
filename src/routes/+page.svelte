97ab9825fbaa32f8b66b1b3abbd82cd366dcfcd4
kazuma1989 <kazuma.ebina@quipper.com>
2023-02-24 15:47:48 +0900

    CSR いけたと思ったけどビルドすると global.css が読み込めない

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script lang="ts">
+  import { onMount } from "svelte"
+
+  let root: HTMLElement
+
+  onMount(async () => {
+    const main = (await import("../main")).default
+
+    main(root)
+  })
+</script>
+
+<div id="root" bind:this={root} />
