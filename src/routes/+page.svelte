480018e36c1b10b4843a010f8b3d729730478f22
kazuma1989 <kazuma.ebina@quipper.com>
2023-02-26 01:11:33 +0900

    main.ts に書く意味がないので routes に bootstrap 処理を持ってきた

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,51 @@
 <script lang="ts">
+  import smallAlert from "$lib/assets/small-alert.mp3"
   import { onMount } from "svelte"
-  import main from "../main"
+  import App from "../App.svelte"
+  import { firestoreImplContext } from "../firestore/firestoreImplContext"
+  import { initRemoteFirestore } from "../firestore/initRemoteFirestore"
+  import { observeAudioPermission } from "../observeAudioPermission"
+  import { observeHash } from "../observeHash"
   import PageRoomSkeleton from "../PageRoomSkeleton.svelte"
   import Skeleton from "../Skeleton.svelte"
+  import {
+    createAudio,
+    keyWithAudio,
+    keyWithMediaPermission,
+  } from "../useAudio"
+  import { keyWithDarkMode, observeDarkMode } from "../useDarkMode"
+  import { createVideoTimer, keyWithVideoTimer } from "../useVideoTimer"
 
   let root: HTMLElement | undefined
+
   onMount(async () => {
-    await main(root!)
+    const darkMode$ = observeDarkMode()
+
+    const context = new AudioContext()
+    const permission$ = observeAudioPermission(context)
+
+    const audioData = await fetch(smallAlert).then((_) => _.arrayBuffer())
+    const audio = createAudio(context, audioData)
+
+    const video = createVideoTimer()
+
+    const route$ = observeHash()
+
+    const firestore = await initRemoteFirestore()
+
+    new App({
+      target: root!,
+      props: {
+        route$,
+      },
+      context: new Map([
+        ...firestoreImplContext(firestore),
+        keyWithAudio(audio),
+        keyWithMediaPermission(permission$),
+        keyWithDarkMode(darkMode$),
+        keyWithVideoTimer(video),
+      ]),
+    })
   })
 </script>
 
