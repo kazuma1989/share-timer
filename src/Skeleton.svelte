85a1d9af531c219b2920d050f7dbbf5ade6f5b82
kazuma1989 <kazuma.ebina@quipper.com>
2023-02-26 02:22:20 +0900

    使わなくなった Skeleton を削除

--- src/Skeleton.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  export let appRoot: HTMLElement | undefined = undefined
-</script>
-
-<div class="peer contents" bind:this={appRoot} />
-<div class="hidden peer-empty:contents">
-  <slot />
-</div>
