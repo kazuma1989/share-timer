bc1e90ef2cd30416271a0219261ed3334831dfff
kazuma1989 <kazuma.ebina@quipper.com>
2023-02-26 02:30:24 +0900

    SplashScreen は不要になったので削除
    pre-render で PageRoomSkeleton が最初に出るから

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import "../global.css"
-  import SplashScreen from "./SplashScreen.svelte"
 </script>
 
 <svelte:head>
@@ -8,19 +7,3 @@
 </svelte:head>
 
 <slot />
-
-<div class="peer-hidden">
-  <SplashScreen />
-</div>
-
-<style>
-  .peer-hidden {
-    transition: 0.3s ease-in;
-    transition-property: opacity, visibility;
-  }
-
-  :global(:not(:empty)) + .peer-hidden {
-    opacity: 0;
-    visibility: hidden;
-  }
-</style>
