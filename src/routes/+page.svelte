0c011791618c95cf30e77fc4c0068fe848729a98
kazuma1989 <kazuma.ebina@quipper.com>
2023-02-26 22:43:25 +0900

    App で持ってた hash routing の機能は全部は不要なので routes/+page でロジックを持つことにした
    newRoom だけじゃなく info, unknown も newRoom 扱いにしておいた

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,18 @@
   import { browser } from "$app/environment"
   import smallAlert from "$lib/assets/small-alert.mp3"
   import { observeAudioPermission } from "$lib/observeAudioPermission"
-  import { observeRoute } from "$lib/observeHash"
+  import { observeRoute, replaceHash } from "$lib/observeHash"
   import SetContext from "$lib/SetContext.svelte"
   import {
     createAudio,
     keyWithAudio,
     keyWithMediaPermission,
   } from "$lib/useAudio"
-  import App from "../App.svelte"
   import { firestoreImplContext } from "../firestore/firestoreImplContext"
   import { initRemoteFirestore } from "../firestore/initRemoteFirestore"
+  import PageRoom from "../PageRoom.svelte"
   import PageRoomSkeleton from "../PageRoomSkeleton.svelte"
+  import { newRoomId } from "../schema/roomSchema"
   import { keyWithDarkMode, observeDarkMode } from "../useDarkMode"
   import { createVideoTimer, keyWithVideoTimer } from "../useVideoTimer"
 
@@ -41,12 +42,24 @@
       keyWithVideoTimer(video),
     ])
   })()
+
+  $: if (browser) {
+    const [route, , mode] = $route$
+
+    if (route !== "room") {
+      replaceHash(["room", newRoomId(mode)])
+    }
+  }
 </script>
 
 <div class="peer contents">
   {#await context$ then context}
     <SetContext {context}>
-      <App {route$} />
+      {#if $route$[0] === "room"}
+        {@const [, roomId] = $route$}
+
+        <PageRoom {roomId} />
+      {/if}
     </SetContext>
   {/await}
 </div>
