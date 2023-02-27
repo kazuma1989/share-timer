52f7d6e13be1692923309fb966fcfbb924f4d341
kazuma1989 <kazuma.ebina@quipper.com>
2023-02-27 22:04:18 +0900

    ifBrowser は使っていないので削除

--- src/lib/ifBrowser.ts
+++
@@ -1,9 +0,0 @@
-import { browser } from "$app/environment"
-import { of, type Observable } from "rxjs"
-
-export function ifBrowser<T>(
-  getter: () => Observable<T>,
-  elseValue: T
-): Observable<T> {
-  return browser ? getter() : of(elseValue)
-}
