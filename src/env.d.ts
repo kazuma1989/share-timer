c9f17b155b03b493f05369cd73dac54421071f79
kazuma1989 <kazuma.ebina@quipper.com>
2023-03-02 10:07:50 +0900

    pricing-table-id を env にした

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,5 +6,6 @@ interface ImportMetaEnv {
   VITE_AUTH_EMULATOR: string
   VITE_DB_VERSION: string
   VITE_FIRESTORE_EMULATOR: string
+  VITE_STRIPE_PRICING_TABLE_ID: string
   VITE_STRIPE_PUBLISHABLE_KEY: string
 }
