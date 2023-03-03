7358ab4fca91126fc59b3822c0b0d46c5e306ac2
kazuma1989 <kazuma.ebina@quipper.com>
2023-03-03 10:16:31 +0900

    Vite の HMR が走ってもそれなりに動作するようにした
    AuthUI インスタンスは global にひとつだけ

--- src/routes/account/sign-in/+page.svelte
+++ src/routes/account/sign-in/+page.svelte
@@ -20,69 +20,78 @@
     src="https://www.gstatic.com/firebasejs/ui/6.0.2/firebase-ui-auth__ja.js"
   ></script>
   <script>
-    const port = Number(
-      new URLSearchParams(window.location.search).get("emulator")
-    )
-    if (port) {
-      const { protocol, hostname } = window.location
+    {
+      const port = Number(
+        new URLSearchParams(window.location.search).get("emulator")
+      )
+      if (port) {
+        const { protocol, hostname } = window.location
 
-      firebase.auth().useEmulator(`${protocol}//${hostname}:${port}`)
+        firebase.auth().useEmulator(`${protocol}//${hostname}:${port}`)
+      }
     }
 
-    customElements.define(
-      "firebaseui-auth",
-      class FirebaseuiAuth extends HTMLElement {
-        constructor() {
-          super()
+    if (!customElements.get("firebaseui-auth")) {
+      customElements.define(
+        "firebaseui-auth",
+        class FirebaseuiAuth extends HTMLElement {
+          constructor() {
+            super()
 
-          const shadowRoot = this.attachShadow({ mode: "open" })
+            this.attachShadow({ mode: "open" })
+          }
 
-          const link = shadowRoot.appendChild(
-            window.document.createElement("link")
-          )
-          link.rel = "stylesheet"
-          link.href =
-            "https://www.gstatic.com/firebasejs/ui/6.0.2/firebase-ui-auth.css"
+          connectedCallback() {
+            const link = this.shadowRoot.appendChild(
+              window.document.createElement("link")
+            )
+            link.rel = "stylesheet"
+            link.href =
+              "https://www.gstatic.com/firebasejs/ui/6.0.2/firebase-ui-auth.css"
 
-          const style = shadowRoot.appendChild(
-            window.document.createElement("style")
-          )
-          style.textContent = `
-            :host {
-              display: block;
-            }
-            button:focus {
-              /* https://css-tricks.com/copy-the-browsers-native-focus-styles/ */
-              outline: Highlight auto medium;
-              outline: -webkit-focus-ring-color auto medium;
-            }
-          `
+            const style = this.shadowRoot.appendChild(
+              window.document.createElement("style")
+            )
+            style.textContent = `
+              :host {
+                display: block;
+              }
+              button:focus {
+                /* https://css-tricks.com/copy-the-browsers-native-focus-styles/ */
+                outline: Highlight auto medium;
+                outline: -webkit-focus-ring-color auto medium;
+              }
+            `
 
-          const ui = new firebaseui.auth.AuthUI(firebase.auth())
-          const container = shadowRoot.appendChild(
-            window.document.createElement("div")
-          )
+            const container = this.shadowRoot.appendChild(
+              window.document.createElement("div")
+            )
 
-          ui.start(container, {
-            signInSuccessUrl:
-              new URLSearchParams(window.location.search).get("back") ||
-              window.location.href,
-            signInOptions: [
-              firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-              // "apple.com",
-              // "microsoft.com",
-              // "yahoo.com",
-              // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-              // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-              // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-              firebase.auth.EmailAuthProvider.PROVIDER_ID,
-              // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-              // firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
-            ],
-          })
+            const ui =
+              firebaseui.auth.AuthUI.getInstance() ??
+              new firebaseui.auth.AuthUI(firebase.auth())
+
+            ui.start(container, {
+              signInSuccessUrl:
+                new URLSearchParams(window.location.search).get("back") ||
+                window.location.href,
+              signInOptions: [
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                // "apple.com",
+                // "microsoft.com",
+                // "yahoo.com",
+                // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+                // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+                // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+                firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+                // firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
+              ],
+            })
+          }
         }
-      }
-    )
+      )
+    }
   </script>
 </svelte:head>
 
